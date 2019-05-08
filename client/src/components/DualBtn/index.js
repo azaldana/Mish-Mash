bddba41b5157c922711624cc2d722a22b688ff4c
Zaldana <alexandrazaldana@gmail.com>
2019-05-07 20:31:01 -0700

    cleaned images folder

---
+++ client/src/components/DualBtn/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+const DualBtn = props => {
+    return (
+
+        <div className="dual-btn">
+        
+        
+        
+        
+        </div>
+
+    );
+};
+
+export default DualBtn;
