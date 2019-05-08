e64d9f6c4105360eef4791dd2c5e191aeef9a1ef
Zaldana <alexandrazaldana@gmail.com>
2019-05-07 18:38:03 -0700

    made react app, added nav, about

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import CardChoices from "./components/CardChoices";
+import Wrapper from "./components/Wrapper";
+// import friends from "./choices.json";
+import NavBar from "./components/NavBar";
+// import Jumbotron from "./components/Jumbotron";
+import About from "./components/About";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+        <NavBar/>
+        <About/>
+      </Wrapper>
   );
 }
 
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./style.css";
+
+const NavBar = props => {
+    return (
+        <div className="container-navbar">
+            <header>
+                <nav class="nav-wrapper transparent z-depth-0">
+                    <a href="#" class="sidenav-trigger" data-target="mobile-menu">
+                        <i class="material-icons white-text">menu</i></a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+
+                        <li><a href="#login" class="white-text login valign-wrapper modal-trigger">Log In</a></li>
+                        <li><a href="#signup" class="white-text signup valign-wrapper modal-trigger">Sign Up</a></li>
+                        <li><a href="#signup" class="white-text recipes valign-wrapper modal-trigger">Recipes</a></li>
+                        <li><a href="#" class="white-text valign-wrapper"></a></li>
+                    </ul>
+
+                    <ul class="sidenav grey lighten-2" id="mobile-menu">
+                        <li><a href="#login" class="blue-grey-text login text-darken-4 modal-trigger">Log In</a></li>
+                        <li><a href="#signup" class="blue-grey-text signup text-darken-4 modal-trigger">Sign Up</a></li>
+                        <li><a href="#" class="blue-grey-text text-darken-4">Recipes</a></li>
+                    </ul>
+                </nav>
+
+                <div class="center-align">
+                    <img src="./images/mish-mash-white.png" width="500" alt="Logo" id="top-logo" />
+                </div>
+            </header>
+        </div>
+    );
+}
+
+export default NavBar;
