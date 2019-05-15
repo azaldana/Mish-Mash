6ba53aa57e66155b85109ae6770dc57805d97199
Nai-Muh Dennis <naimuhdennis@gmail.com>
2019-05-14 19:32:05 -0700

    mashup of homepage working & app.js routes setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,70 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import AllChefs from "./pages/AllChefs";
+import Blogs from "./pages/Blogs";
+import Categories from "./pages/Categories";
 import Home from "./pages/Home";
-import Recipe from "./pages/Recipes";
-import Wrapper from "./components/Wrapper";
+import OneChef  from "./pages/OneChef";
+import Recipes from "./pages/Recipes";
 
 
-function App() {
+
+// function App() {
+//   return (
+//     <Home></Home>
+//   )
+// }
+
+function Index() {
   return (
-    <Wrapper>
-
-      <Home></Home>
-      {/* <Recipe></Recipe> */}
-
-
-    </Wrapper>
-
+    <Home></Home>
   )
 }
 
+function Featured() {
+  return (
+    <AllChefs></AllChefs>
+  )
+}
+
+function Articles() {
+  return (
+    <Blogs></Blogs>
+  )
+}
+
+function Ingredients() {
+  return (
+    <Categories></Categories>
+  )
+}
+
+function Meal() {
+  return (
+    <Recipes></Recipes>
+  )
+}
+
+function App() {
+  return (
+    <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/allchefs" component={AllChefs} />
+            <Route exact path="/blogs" component={Blogs} />
+            <Route exact path="/categories" component={Categories} />
+            <Route exact path="/onechef" component={OneChef} />
+            <Route exact path="/recipes" component={Recipes} />
+            {/* <Route component={NoMatch} /> */}
+          </Switch>
+        </div>
+    </Router>
+  );
+}
+
 export default App;
+
+
+
+// export default App;
