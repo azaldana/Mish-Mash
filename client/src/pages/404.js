e5195b3eb433f7bb0d9119e5ec272a9b70681fa7
Zaldana <alexandrazaldana@gmail.com>
2019-05-21 17:32:29 -0700

    404 page update

--- client/src/pages/404.js
+++ client/src/pages/404.js
@@ -1,9 +1,15 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import NavBarAllChefs from "../components/AllChefs/NavBar";
+import Wrapper from "../components/Wrapper";
+import Footer from "../components/Home/Footer";
 
 function AllChefs() {
-    return (
-        <NavBarAllChefs/>
-    );
-  }
+  return (
+    <Wrapper>
+      <NavBarAllChefs />
+      <Footer />
+    </Wrapper>
+
+  );
+}
 export default AllChefs;
