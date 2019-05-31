acad046cc0a26b2ddfb005d2431f2f86f52f039f
Nai-Muh Dennis <naimuhdennis@gmail.com>
2019-05-30 17:13:28 -0700

    work bitch

--- routes/api-routes/recipes.js
+++ routes/api-routes/recipes.js
@@ -6,5 +6,9 @@ const recipesController = require('../..controllers/recipesController');
 // Recipes routes
 router.route('/recipes/:query')
     .get(recipesController.findAll);
-
+router.routes('/recipes')
+    .post(recipesController.getRecipes);
+// router.post('/recipes', function (req, res) {
+//     console.log(req.body);
+// })
 module.exports = router;
