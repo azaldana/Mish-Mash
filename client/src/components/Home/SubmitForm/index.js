ac1f11526e867b93b0d872c036096f1f1059b4a5
Zaldana <alexandrazaldana@gmail.com>
2019-06-01 11:06:07 -0700

    login connected with the submit form

--- client/src/components/Home/SubmitForm/index.js
+++ client/src/components/Home/SubmitForm/index.js
@@ -1,11 +1,62 @@
 import React from "react";
-import { Textarea, TextInput, Modal, Button } from 'react-materialize';
+import { Textarea, TextInput, Modal, Button } from "react-materialize";
 import "./style.css";
 import API from "../../../utils/API";
 // import Axios from "axios";
 
 class SubmitForm extends React.Component {
-    state = {
+  state = {
+    title: "",
+    ingredients: "",
+    instructions: "",
+    totalTime: "",
+    servings: "",
+    social: "",
+    image: null,
+    image_id: "",
+    isSubmitFormModalOpen: false
+  };
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFileChange = event => {
+    const { files, value } = event.target;
+    console.log(files[0]);
+    console.log(value);
+    this.setState({
+      image: files[0]
+    });
+  };
+
+  handleSubmitForm = () => {
+    const {
+      title,
+      ingredients,
+      instructions,
+      totalTime,
+      servings,
+      social,
+      image,
+      image_id
+    } = this.state;
+    const { submission } = this.props;
+    API.submitForm({
+      title,
+      ingredients,
+      instructions,
+      totalTime,
+      servings,
+      social,
+      image,
+      image_id
+    }).then(res => {
+      alert("Recipe submitted, thank you!");
+      this.setState({
+        isSubmitFormModalOpen: false,
         title: "",
         ingredients: "",
         instructions: "",
@@ -13,171 +64,174 @@ class SubmitForm extends React.Component {
         servings: "",
         social: "",
         image: null,
-        image_id: "",
-        isSubmitFormModalOpen: false,
-    }
-    handleChange = (event) => {
-        const { name, value } = event.target;
-        this.setState({
-            [name]: value
-        });
-    }
+        image_id: ""
+      });
+    });
+  };
 
-    handleFileChange = (event) => {
-        const { files, value } = event.target;
-        console.log(files[0]);
-        console.log(value);
-        this.setState({
-            image: files[0]
-        })
-    }
+  openSubmitFormModal = event => {
+    event.preventDefault();
+    this.setState({
+      isSubmitFormModalOpen: true
+    });
+  };
 
-    handleSubmitForm = () => {
-        const { title, ingredients, instructions, totalTime, servings, social, image, image_id } = this.state;
-        const { submission } = this.props;
-        API.submitForm({ title, ingredients, instructions, totalTime, servings, social, image, image_id })
-            .then(res => {
-                alert("Recipe submitted, thank you!");
-                this.setState({
-                    isSubmitFormModalOpen: false,
-                    title: "",
-                    ingredients: "",
-                    instructions: "",
-                    totalTime: "",
-                    servings: "",
-                    social: "",
-                    image: null,
-                    image_id: "",
-                })
-            });
-    }
+  closeSubmitFormModal = () => {
+    this.setState({
+      isSubmitFormModalOpen: false
+    });
+  };
 
-    openSubmitFormModal = (event) => {
-        event.preventDefault();
-        this.setState({
-            isSubmitFormModalOpen: true
-        });
-    }
+  render() {
+    const {
+      title,
+      ingredients,
+      instructions,
+      totalTime,
+      servings,
+      social
+    } = this.state;
+    const { submission } = this.props;
+    const { user } = this.props;
+    // console.log(submission);
+    return (
+      <div className="submit-form" id="submit-form">
+        <h2>Family Recipes</h2>
 
-    closeSubmitFormModal = () => {
-        this.setState({
-            isSubmitFormModalOpen: false
-        })
-    }
+        <img
+          src="./images/mish-mash-icon-yellow-new.png"
+          width="50"
+          alt="Icon"
+          id="submitIcon"
+        />
+        <br />
 
+        <p className="subhead">
+          Are you the Chef Ramsay of your friends, and reign supreme in the
+          kitchen? <br />
+          Place your bid in for Top Chef -- Submit, Rate Share!
+        </p>
+        <br />
+        <img
+          className="mockup-recipe"
+          src="./images/recipe_layout.jpg"
+          width="1000"
+          alt="Recipe Page Mockup"
+        />
 
-    render() {
-        const { title, ingredients, instructions, totalTime, servings, social } = this.state;
-        const { submission } = this.props;
-        // console.log(submission);
-        return (
-            <div className="submit-form" id="submit-form">
-                <h2>Family Recipes</h2>
+        {user ? (
+          <Button
+            className="submit-form-button"
+            onClick={this.openSubmitFormModal}
+          >
+            Submit Your Recipe
+          </Button>
+        ) : null}
 
-                <img src="./images/mish-mash-icon-yellow-new.png" width="50" alt="Icon" id="submitIcon" />
-                <br></br>
+        {this.state.isSubmitFormModalOpen ? (
+          <Modal
+            header="Submit Your Family Recipe"
+            className="black-text submission"
+            open={true}
+          >
+            <br />
+            <TextInput
+              icon="restaurant_menu"
+              name="title"
+              value={title}
+              // label="Title"
+              placeholder="Title"
+              id="form_title"
+              type="text"
+              className="validate"
+              onChange={this.handleChange}
+            />
 
-                <p className="subhead">Are you the Chef Ramsay of your friends, and reign supreme in the kitchen? <br></br>
-                Place your bid in for Top Chef -- Submit, Rate Share!</p><br></br>
-                <img className="mockup-recipe" src="./images/recipe_layout.jpg" width="1000" alt="Recipe Page Mockup" />
+            <Textarea
+              icon="list"
+              name="ingredients"
+              value={ingredients}
+              // label="Ingredients"
+              placeholder="Ingredients"
+              className="validate"
+              data-length={150}
+              onChange={this.handleChange}
+            />
 
-                {submission ? null : (
-                    <Button className="submit-form-button" onClick={this.openSubmitFormModal}>Submit Your Recipe</Button>
-                )}
+            <Textarea
+              icon="format_list_numbered"
+              name="instructions"
+              value={instructions}
+              // label="Instructions"
+              placeholder="Instructions"
+              className="validate instructions"
+              data-length={350}
+              onChange={this.handleChange}
+            />
 
-                {this.state.isSubmitFormModalOpen ? (
-                    <Modal header="Submit Your Family Recipe" className="black-text submission" open={true}>
-                        <br></br>
-                        <TextInput
-                            icon="restaurant_menu"
-                            name="title"
-                            value={title}
-                            // label="Title"
-                            placeholder="Title"
-                            id="form_title"
-                            type="text"
-                            className="validate"
-                            onChange={this.handleChange}
-                        />
+            <TextInput
+              icon="query_builder"
+              name="totalTime"
+              value={totalTime}
+              // label="Total Time"
+              placeholder="Total Time"
+              id="totalTime"
+              type="text"
+              className="validate"
+              onChange={this.handleChange}
+            />
 
-                        <Textarea
-                            icon="list"
-                            name="ingredients"
-                            value={ingredients}
-                            // label="Ingredients"
-                            placeholder="Ingredients"
-                            className="validate"
-                            data-length={150}
-                            onChange={this.handleChange}
-                        />
+            <TextInput
+              icon="clear_all"
+              name="servings"
+              value={servings}
+              // label="Servings"
+              placeholder="Servings"
+              id="servings"
+              type="text"
+              className="validate"
+              onChange={this.handleChange}
+            />
 
-                        <Textarea
-                            icon="format_list_numbered"
-                            name="instructions"
-                            value={instructions}
-                            // label="Instructions"
-                            placeholder="Instructions"
-                            className="validate instructions"
-                            data-length={350}
-                            onChange={this.handleChange}
-                        />
+            <TextInput
+              icon="tag_faces"
+              name="social"
+              value={social}
+              // label="Social"
+              placeholder="Social"
+              id="social"
+              type="text"
+              className="validate"
+              onChange={this.handleChange}
+            />
 
-                        <TextInput
-                            icon="query_builder"
-                            name="totalTime"
-                            value={totalTime}
-                            // label="Total Time"
-                            placeholder="Total Time"
-                            id="totalTime"
-                            type="text"
-                            className="validate"
-                            onChange={this.handleChange}
-                        />
+            <input
+              name="image"
+              type="file"
+              className="validate"
+              onChange={this.handleFileChange}
+            />
+            <br />
 
-                        <TextInput
-                            icon="clear_all"
-                            name="servings"
-                            value={servings}
-                            // label="Servings"
-                            placeholder="Servings"
-                            id="servings"
-                            type="text"
-                            className="validate"
-                            onChange={this.handleChange}
-                        />
-
-                        <TextInput
-                            icon="tag_faces"
-                            name="social"
-                            value={social}
-                            // label="Social"
-                            placeholder="Social"
-                            id="social"
-                            type="text"
-                            className="validate"
-                            onChange={this.handleChange}
-                        />
-
-                        <input
-                            name="image"
-                            type="file"
-                            className="validate"
-                            onChange={this.handleFileChange} />
-                            <br></br>
-
-                        <Button className="submit-form-button" onClick={this.handleSubmitForm}>Submit
-                        <i class="material-icons right">send</i>
-                        </Button>
-                        <Button
-                                onClick={this.closeSubmitFormModal}
-                                className="submit-form-button">Close
-                                <i class="material-icons right">close</i>
-                            </Button>
-                    </Modal>
-                ) : null}
-            </div>
-        );
-    }
+            <Button
+              className="submit-form-button"
+              onClick={this.handleSubmitForm}
+            >
+              Submit
+              <i class="material-icons right">send</i>
+            </Button>
+            <Button
+              onClick={this.closeSubmitFormModal}
+              className="submit-form-button"
+            >
+              Close
+              <i class="material-icons right">close</i>
+            </Button>
+          </Modal>
+        ) : null}
+      </div>
+    );
+  }
 }
 
 export default SubmitForm;
