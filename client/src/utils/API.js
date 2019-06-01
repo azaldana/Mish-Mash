d66477a7a962fd88210eb6e230ea08dfb867d64d
Zaldana <alexandrazaldana@gmail.com>
2019-06-01 13:28:37 -0700

    cloudinary works

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,46 +1,29 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   // Gets all posts
   getPosts: function() {
-    return axios.get('/api/posts');
+    return axios.get("/api/posts");
   },
   createPost: function(data) {
-    return axios.post('/api/posts', data);
+    return axios.post("/api/posts", data);
   },
   signup: function(data) {
-    return axios.post('/api/users/signup', data);
+    return axios.post("/api/users/signup", data);
   },
   login: function(data) {
-    return axios.post('/api/users/login', data);
+    return axios.post("/api/users/login", data);
   },
   validateToken: function(t) {
-    return axios.post('/api/users/validate', { token: t });
+    return axios.post("/api/users/validate", { token: t });
   },
-  submitForm: function(data){
-
-    // return axios.post('/api/form/create', data);
-    const formData = new FormData();
-    const { title, ingredients, instructions, totalTime, servings, social, image, image_id } = data;
-    formData.append('title', title);
-    formData.append('ingredients', ingredients);
-    formData.append('instructions', instructions);
-    formData.append('totalTime', totalTime);
-    formData.append('servings', servings);
-    formData.append('social', social);
-    formData.append('image', image);
-    formData.append('image_id', image_id );
-    return axios.post('/api/form/create', data, {
-      // headers: {
-      //   "Content-type": "multiform/form-data"
-      // }
-    });
+  submitForm: function(data) {
+    return axios.post("/api/form/create", data);
   },
   getCategories: function(query) {
-    return axios.post('/api/categories', query);
+    return axios.post("/api/categories", query);
   },
   getRecipes: function(query) {
-    return axios.post('/api/recipes', query);
-  },
-  
+    return axios.post("/api/recipes", query);
+  }
 };
